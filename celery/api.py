89a494d4f62e44373b770c20f19f897ec636fc7c
root <root@asus-rog-admin.>
2025-04-19 00:32:22 +0300

    q

--- celery/api.py
+++ celery/api.py
@@ -4,11 +4,17 @@ from producer import push
 
 app = FastAPI()
 
-UPLOAD_DIR = "/app/data"
+UPLOAD_DIR = "/app/"
+
+@app.get("/test")
+async def test():
+    return os.listdir(UPLOAD_DIR)
+
 
 @app.post("/process/")
 async def process_file(filename: str = Query(..., description="Имя файла, который заранее загружен в общий каталог")):
     file_path = os.path.join(UPLOAD_DIR, filename)
+    print(os.listdir(UPLOAD_DIR))
     if not os.path.exists(file_path):
         raise HTTPException(status_code=400, detail="Файл не найден в общем каталоге.")
     
--- celery/entrypoint.sh
+++ celery/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-set -e
-
-
-uvicorn api:app --host 0.0.0.0 --port 8000 &
-
-
-celery -A celery.app worker --loglevel=info --concurrency=4
+# Запускаем celery worker в фоне, скрывая его логи
+celery -A app worker --loglevel=info --concurrency=4 > /dev/null 2>&1 &
+# Запускаем uvicorn в фореграунде – его логи будут выводиться в терминал
+uvicorn api:app --host 0.0.0.0 --port 8000
